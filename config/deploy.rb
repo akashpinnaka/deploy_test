7d5849a963e658f93d3d6757fe8003113afb03ed
akashpinnaka <aakash.pinnaka@gmail.com>
2017-05-01 23:50:49 +0530

    Change user name in capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@example.com:akashpinnaka/deplot_test.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/ubuntu/deplot_test"
+set :deploy_to, "/home/deploy/deplot_test"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh

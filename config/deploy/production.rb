34ddc03cd69d650f60977c7a86c4be3cdb0866e7
akashpinnaka <aakash.pinnaka@gmail.com>
2017-05-01 22:58:58 +0530

    Add remote server url to config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '52.88.191.158', user: 'ubuntu', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
